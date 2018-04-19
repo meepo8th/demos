8b519491dabd5e8598c3a9c99e83d081a6962735
zhangmeng <3202283072@qq.com>
2018-04-19 10:12:18 +0800

    增加文档

--- src/main/leetcode/findtargetsumway/Solution.java
+++ src/main/leetcode/findtargetsumway/Solution.java
@@ -1,11 +1,40 @@
 package leetcode.findtargetsumway;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int findTargetSumWays(int[] nums, int S) {
-        return 0;
+        Map<Integer, Integer>[] cache = new HashMap[nums.length];
+        cache[0] = new HashMap<>();
+        putValue(cache[0], nums[0], 1);
+        putValue(cache[0], nums[0] * -1, 1);
+        for (int i = 1; i < nums.length; i++) {
+            Map<Integer, Integer> map = cache[i - 1];
+            cache[i] = new HashMap<>();
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                int num1 = entry.getKey() + nums[i];
+                int num2 = entry.getKey() - nums[i];
+                putValue(cache[i], num1, entry.getValue());
+                putValue(cache[i], num2, entry.getValue());
+            }
+        }
+        return cache[nums.length - 1].containsKey(S) ? cache[nums.length - 1].get(S) : 0;
     }
 
-    public static void main(String[] args) {
+    private void putValue(Map<Integer, Integer> map, int num, int value) {
+        if (!map.containsKey(num)) {
+            map.put(num, value);
+        } else {
+            map.put(num, value + map.get(num));
+        }
 
+    }
+
+
+    public static void main(String[] args) {
+        long l1 = System.currentTimeMillis();
+        System.out.println(new Solution().findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
+        System.out.println(System.currentTimeMillis() - l1);
     }
 }
