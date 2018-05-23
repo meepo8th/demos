e2c2e737f9f1ebad5b8614698a09374422168056
zhangmeng <3202283072@qq.com>
2018-05-23 15:29:11 +0800

    增加文档

---
+++ src/main/demo/A.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class A {
+    static {
+        System.out.println("A class init");
+    }
+
+    public static final String HELLO = "hello";
+
+
+}
---
+++ src/main/demo/Main.java
@@ -0,0 +1,6 @@
+package demo;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new A().HELLO);
+    }
+}
