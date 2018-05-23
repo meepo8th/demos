b4c5b1c3af9f29d0acb13eca9ba63034b305717e
zhangmeng <3202283072@qq.com>
2018-05-23 15:14:02 +0800

    增加文档

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,35 +1,55 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Solution {
-    public String convert(String s, int numRows) {
-        String rtn = "";
-
-        return rtn;
-    }
-
-    public Number get3() {
-        return new Number() {
-            @Override
-            public int intValue() {
-                return 3;
+    private String parseOption(String pathWay) {
+        File file = new File(pathWay);
+        BufferedReader br = null;
+        StringBuffer sb=new StringBuffer();
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String str;
+            while ((str = br.readLine()) != null) {
+                sb.append(str);
             }
+            String contentHtml = sb.toString();
+            String pattern = "(var +option +=.+)var +bag +=";
 
-            @Override
-            public long longValue() {
-                return 3;
-            }
+            // 创建 Pattern 对象
+            Pattern r = Pattern.compile(pattern);
 
-            @Override
-            public float floatValue() {
-                return 3;
+            // 现在创建 matcher 对象
+            Matcher m = r.matcher(contentHtml);
+            if (m.find( )) {
+                String content=m.group(1).trim().replaceAll(";$","");
+                content=content.substring(content.indexOf("=")+1);
+                JSONObject object = JSON.parseObject(content);
+                System.out.println(object.get("result") );
+            } else {
+                System.out.println("no match");
             }
-
-            @Override
-            public double doubleValue() {
-                return 3;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        };
+        }
+        return "";
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().get3().intValue());
+        System.out.println(new Solution().parseOption("f:/123.txt"));
     }
 }
