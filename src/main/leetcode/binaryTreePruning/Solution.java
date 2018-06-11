77126c00fa671fd7a21ad51fdedf9a1e4646b599
zhangmeng <94739473@qq.com>
2018-06-11 22:15:59 +0800

    灯泡开关 & 二叉树剪枝

--- src/main/leetcode/binaryTreePruning/Solution.java
+++ src/main/leetcode/binaryTreePruning/Solution.java
@@ -1,22 +1,48 @@
 package leetcode.binaryTreePruning;
 
 
+import java.util.Stack;
+
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
-        removeZeroNode(root);
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack.push(root);
+        stack2.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (null != node) {
+                stack.push(node.left);
+                stack.push(node.right);
+                stack2.push(node.left);
+                stack2.push(node.right);
+            }
+        }
+        while (!stack2.isEmpty()) {
+            TreeNode node = stack2.pop();
+            if (node != null  ) {
+                if(null!=node.left) {
+                    if (node.left.val == 0 && node.left.left == null && node.left.right == null) {
+                        node.left = null;
+                    }
+                }
+                if(null!=node.right) {
+                    if (node.right.val == 0 && node.right.left == null && node.right.right == null) {
+                        node.right = null;
+                    }
+                }
+            }
+        }
         return root;
     }
 
-    private void removeZeroNode(TreeNode root) {
-        if (null != root) {
-            if (root.right == null && root.left == null) {
-                if (root.val == 0) {
-                    root = null;
-                }
-            } else {
-                removeZeroNode(root.left);
-                removeZeroNode(root.right);
-            }
-        }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = null;
+        root.right = new TreeNode(0);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(1);
+        System.out.println(new Solution().pruneTree(root));
     }
+
 }
--- src/main/leetcode/bulbSwitch/Solution.java
+++ src/main/leetcode/bulbSwitch/Solution.java
@@ -1,34 +1,13 @@
 package leetcode.bulbSwitch;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int bulbSwitch(int n) {
-        int count = 0;
-        Map<Integer, Integer> cache = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-            int now = 0;
-            for (int j = 1; j * j <= i; j++) {
-                if (i % j == 0) {
-                    if (j * j == i) {
-                        now += 1;
-                    } else {
-                        now += 2;
-                    }
-                }
-            }
-            cache.put(i, now);
-            if (now % 2 == 1) {
-                count++;
-            }
-        }
-        return count;
+        return (int) Math.sqrt(n);
     }
 
     public static void main(String[] args) {
         long l1 = System.currentTimeMillis();
-        System.out.println(new Solution().bulbSwitch(999999));
+        System.out.println(new Solution().bulbSwitch(99999999));
         System.out.println(System.currentTimeMillis() - l1);
     }
 }
