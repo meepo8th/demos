85a5b35d092c8d7fd8b204fe6e57e4a4c7c09bdd
zhangmeng <zhangmeng@zhangmeng>
2018-06-19 09:30:58 +0800

    山峰数组

---
+++ src/main/leetcode/peakindexinmountainarray/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.peakindexinmountainarray;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int last=Integer.MIN_VALUE;
+        for(int i=0;i<A.length;i++){
+            if(A[i]<last){
+                return i-1;
+            }
+            last=A[i];
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        System.out.println(new Solution().peakIndexInMountainArray(new int[]{0,2,1,0}));
+    }
+}
