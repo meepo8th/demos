ee6647dfb65e22b2141012dcd4378f51ac9ceb76
zhangmeng <zhangmeng@zhangmeng>
2018-06-21 15:59:34 +0800

    山峰数组

---
+++ src/main/leetcode/carfleet/Solution.java
@@ -0,0 +1,43 @@
+package leetcode.carfleet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    class Car implements Comparable<Car> {
+        Integer pos;
+        Integer speed;
+
+        public Car(int pos, int speed) {
+            this.pos = pos;
+            this.speed = speed;
+        }
+
+        @Override
+        public int compareTo(Car o) {
+            return pos.compareTo(o.pos);
+        }
+    }
+
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < position.length; i++) {
+            cars.add(new Car(position[i], speed[i]));
+        }
+        int time = 10 ^ 6;
+        int fleet = 0;
+        for (int i = 0; i < cars.size(); i++) {
+            if ((target - cars.get(i).pos) < cars.get(i).speed * time) {
+                fleet++;
+                time = Math.min(time, (target - cars.get(i).pos) / cars.get(i).speed);
+            } else {
+                break;
+            }
+        }
+        return fleet;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().carFleet(12, new int[]{10, 8, 0, 5, 3}, new int[]{2, 4, 1, 1, 3}));
+    }
+}
---
+++ src/main/leetcode/loudandrich/Solution.java
@@ -0,0 +1,63 @@
+package leetcode.loudandrich;
+
+import java.util.*;
+
+public class Solution {
+    public int[] loudAndRich(int[][] richer, int[] quiet) {
+        int[] rtn = new int[quiet.length];
+        Richer[] mapRicher = new Richer[quiet.length];
+        Richer[] richerMin = new Richer[quiet.length];
+        for (int i = 0; i < quiet.length; i++) {
+            mapRicher[i] = new Richer(i, quiet[i]);
+        }
+        for (int[] compare : richer) {
+            mapRicher[compare[1]].child.add(mapRicher[compare[0]]);
+        }
+        for (int i = 0; i < quiet.length; i++) {
+            rtn[i] = minQuiet(mapRicher[i], richerMin).id;
+        }
+        return rtn;
+    }
+
+    /**
+     * Richer
+     *
+     * @param richer
+     * @param richerMin
+     * @return
+     */
+    private Richer minQuiet(Richer richer, Richer[] richerMin) {
+        if (null != richerMin[richer.id]) {
+            return richerMin[richer.id];
+        }
+        Richer minRicher = richer;
+        Richer childMin;
+        for (Richer child : richer.child) {
+            if (child.child.isEmpty()) {
+                childMin = child;
+            } else {
+                childMin = minQuiet(child, richerMin);
+            }
+            if (minRicher.quiet > childMin.quiet) {
+                minRicher = childMin;
+            }
+        }
+        richerMin[richer.id] = minRicher;
+        return minRicher;
+    }
+
+    class Richer {
+        int id;
+        int quiet;
+        List<Richer> child = new ArrayList<>(10);
+
+        public Richer(int id, int quiet) {
+            this.id = id;
+            this.quiet = quiet;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().loudAndRich(new int[][]{{1, 0}, {2, 1}, {3, 1}, {3, 7}, {4, 3}, {5, 3}, {6, 3}}, new int[]{3, 2, 5, 4, 6, 1, 7, 0})));
+    }
+}
