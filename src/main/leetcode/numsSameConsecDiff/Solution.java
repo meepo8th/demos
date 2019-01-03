c1e2b0c367f170e3d1ce093fdbd5492ec6059a18
zhangmeng <zhangmeng@bikeman.info>
2019-01-03 15:47:36 +0800

    Default Changelist

--- src/main/leetcode/numsSameConsecDiff/Solution.java
+++ src/main/leetcode/numsSameConsecDiff/Solution.java
@@ -3,88 +3,58 @@ package leetcode.numsSameConsecDiff;
 import java.util.*;
 
 class Solution {
-    static ArrayList<Map<Integer,ArrayList<Integer>>>[] cache = new ArrayList[10];
 
-    static {
-        for (int i = 0; i < 10; i++) {
-            cache[i] = new ArrayList<>();
-            for (int j = 0; j < 10; j++) {
-                if (j + i < 10) {
-//                    cache[i].add(new StartEnd(j, j + i));
-//                    cache[i].add(new StartEnd(j + i, j));
+
+    public int[] numsSameConsecDiff(int N, int K) {
+
+        List<Node<Integer,Integer>>[] lists = new ArrayList[N];
+        lists[0] = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            lists[0].add(new Node<>(i,i));
+        }
+        for (int i = 1; i < N; i++) {
+            lists[i] = new ArrayList<>();
+            for (Node<Integer,Integer> now : lists[i - 1]) {
+                if(now.getValue()>0) {
+                    if (now.getKey() + K < 10 ) {
+                        lists[i].add(new Node( now.getKey() + K,now.getValue() * 10 + now.getKey() + K));
+                    }
+                    if (K>0&&now.getKey() - K >= 0) {
+                        lists[i].add(new Node( now.getKey() - K,now.getValue() * 10 + now.getKey() - K));
+                    }
                 }
             }
         }
-    }
-
-    public int[] numsSameConsecDiff(int N, int K) {
-        Set<Integer> rtn = new HashSet<>();
-        if(N==1){
-            return new int[]{0,1,2,3,4,5,6,7,8,9};
-        }
-//        for (StartEnd startEnds : cache[K]) {
-//            int tmp = 0;
-//            for (int i = 0; i < N; i++) {
-//                if (i % 2 == 0) {
-//                    tmp = (tmp * 10) + startEnds.getStart();
-//                } else {
-//                    tmp = (tmp * 10) + startEnds.getEnd();
-//                }
-//            }
-//            if (String.valueOf(tmp).length() == N) {
-//                rtn.add(tmp);
-//            }
-//        }
-        int[] rtnArray = new int[rtn.size()];
         int i = 0;
-        for (Integer integer : rtn) {
-            rtnArray[i++] = integer;
+        int[] rtnArray = new int[lists[N-1].size()];
+        for (Node<Integer,Integer> node : lists[N-1]) {
+            rtnArray[i++] = node.getValue();
         }
         return rtnArray;
     }
+    private class Node<K,V> {
+        K key;
+        V value;
 
-    static class StartEnd {
-        public StartEnd(int start, int end) {
-            this.start = start;
-            this.end = end;
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
 
-        int start;
-        int end;
-
-        public int getStart() {
-            return start;
+        public K getKey() {
+            return key;
         }
 
-        public void setStart(int start) {
-            this.start = start;
+        public void setKey(K key) {
+            this.key = key;
         }
 
-        public int getEnd() {
-            return end;
+        public V getValue() {
+            return value;
         }
 
-        public void setEnd(int end) {
-            this.end = end;
-        }
-
-        @Override
-        public int hashCode() {
-            return start * 31 + end;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            StartEnd startEnd = (StartEnd) o;
-            return null != o && startEnd.getStart() == getStart() && startEnd.getEnd() == getEnd();
-        }
-
-        @Override
-        public String toString() {
-            return "StartEnd{" +
-                    "start=" + start +
-                    ", end=" + end +
-                    '}';
+        public void setValue(V value) {
+            this.value = value;
         }
     }
 
@@ -94,4 +64,6 @@ class Solution {
         System.out.println(Arrays.toString(new Solution().numsSameConsecDiff(1, 6)));
         System.out.println(Arrays.toString(new Solution().numsSameConsecDiff(3, 1)));
     }
+
+
 }
