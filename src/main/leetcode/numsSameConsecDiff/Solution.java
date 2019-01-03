a0bb2e576fbc2e67636bdb3c5d2d25dc0cf746d2
zhangmeng <zhangmeng@bikeman.info>
2019-01-03 16:29:36 +0800

    Default Changelist

--- src/main/leetcode/numsSameConsecDiff/Solution.java
+++ src/main/leetcode/numsSameConsecDiff/Solution.java
@@ -1,38 +1,41 @@
 package leetcode.numsSameConsecDiff;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
 
 
     public int[] numsSameConsecDiff(int N, int K) {
 
-        List<Node<Integer,Integer>>[] lists = new ArrayList[N];
+        List<Node<Integer, Integer>>[] lists = new ArrayList[N];
         lists[0] = new ArrayList<>();
-        for(int i=0;i<10;i++){
-            lists[0].add(new Node<>(i,i));
+        for (int i = 0; i < 10; i++) {
+            lists[0].add(new Node<>(i, i));
         }
         for (int i = 1; i < N; i++) {
             lists[i] = new ArrayList<>();
-            for (Node<Integer,Integer> now : lists[i - 1]) {
-                if(now.getValue()>0) {
-                    if (now.getKey() + K < 10 ) {
-                        lists[i].add(new Node( now.getKey() + K,now.getValue() * 10 + now.getKey() + K));
+            for (Node<Integer, Integer> now : lists[i - 1]) {
+                if (now.getValue() > 0) {
+                    if (now.getKey() + K < 10) {
+                        lists[i].add(new Node(now.getKey() + K, now.getValue() * 10 + now.getKey() + K));
                     }
-                    if (K>0&&now.getKey() - K >= 0) {
-                        lists[i].add(new Node( now.getKey() - K,now.getValue() * 10 + now.getKey() - K));
+                    if (K > 0 && now.getKey() - K >= 0) {
+                        lists[i].add(new Node(now.getKey() - K, now.getValue() * 10 + now.getKey() - K));
                     }
                 }
             }
         }
         int i = 0;
-        int[] rtnArray = new int[lists[N-1].size()];
-        for (Node<Integer,Integer> node : lists[N-1]) {
+        int[] rtnArray = new int[lists[N - 1].size()];
+        for (Node<Integer, Integer> node : lists[N - 1]) {
             rtnArray[i++] = node.getValue();
         }
         return rtnArray;
     }
-    private class Node<K,V> {
+
+    private class Node<K, V> {
         K key;
         V value;
 
