e4ef6259755b9e721ce9882900989e810df07452
zhangmeng <zhangmeng@bikeman.info>
2019-01-03 16:47:17 +0800

    优化效率

--- src/main/leetcode/spellchecker/Solution.java
+++ src/main/leetcode/spellchecker/Solution.java
@@ -17,30 +17,49 @@ class Solution {
         rtn.add(new HashMap<>());
         rtn.add(new HashMap<>());
         rtn.add(new HashMap<>());
-        for(String word:wordlist){
-            if(!rtn.get(0).containsKey(word)){
-                rtn.get(0).put(word,word);
+        String[] words ;
+        for (String word : wordlist) {
+            words = preCache(word);
+            if (!rtn.get(0).containsKey(word)) {
+                rtn.get(0).put(word, word);
             }
-            if(!rtn.get(1).containsKey(word.toLowerCase())){
-                rtn.get(1).put(word.toLowerCase(),word);
+            if (!rtn.get(1).containsKey(words[0])) {
+                rtn.get(1).put(words[0], word);
             }
-            if(!rtn.get(2).containsKey(word.toLowerCase().replaceAll("[aeiou]","i"))){
-                rtn.get(2).put(word.toLowerCase().replaceAll("[aeiou]","i"),word);
+            if (!rtn.get(2).containsKey(words[1])) {
+                rtn.get(2).put(words[1], word);
             }
         }
         return rtn;
     }
 
-    private String find(String query, List<Map<String, String>> preCache) {
-            if (preCache.get(0).containsKey(query)) {
-                return preCache.get(0).get(query);
-            }else{
-                if(preCache.get(1).containsKey(query.toLowerCase())){
-                    return preCache.get(1).get(query.toLowerCase());
-                }else if(preCache.get(2).containsKey(query.toLowerCase().replaceAll("[aeiou]","i"))){
-                    return preCache.get(2).get(query.toLowerCase().replaceAll("[aeiou]","i"));
-                }
+    private String[] preCache(String word) {
+        StringBuilder lower = new StringBuilder();
+        StringBuilder yuan = new StringBuilder();
+        char low;
+        for (char ch : word.toCharArray()) {
+            low = Character.toLowerCase(ch);
+            lower.append(low);
+            if (low == 'a' || low == 'e' || low == 'i' || low == 'o' || low == 'u') {
+                yuan.append('a');
+            } else {
+                yuan.append(low);
             }
+        }
+        return new String[]{lower.toString(), yuan.toString()};
+    }
+
+    private String find(String query, List<Map<String, String>> preCache) {
+        if (preCache.get(0).containsKey(query)) {
+            return preCache.get(0).get(query);
+        } else {
+            String[] words = preCache(query);
+            if (preCache.get(1).containsKey(words[0])) {
+                return preCache.get(1).get(words[0]);
+            } else if (preCache.get(2).containsKey(words[1])) {
+                return preCache.get(2).get(words[1]);
+            }
+        }
 
         return "";
     }
