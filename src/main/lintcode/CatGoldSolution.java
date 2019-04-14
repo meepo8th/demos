9b85cc9e59b43d6160a8669809a712fb37e427db
zhangmeng <94739473@qq.com>
2019-04-14 21:08:58 +0800

    lintcode cat

--- src/main/lintcode/CatGoldSolution.java
+++ src/main/lintcode/CatGoldSolution.java
@@ -172,8 +172,175 @@ public class CatGoldSolution {
         return null;
     }
 
+    /**
+     * 153. 数字组合 II
+     * 中文English
+     * 给出一组候选数字(C)和目标数字(T),找出C中所有的组合，使组合中数字的和为T。C中每个数字在每个组合中只能使用一次。
+     * <p>
+     * 样例
+     * 给出一个例子，候选数字集合为[10,1,6,7,2,1,5] 和目标数字 8  ,
+     * <p>
+     * 解集为：[[1,7],[1,2,5],[2,6],[1,1,6]]
+     * <p>
+     * 注意事项
+     * 所有的数字(包括目标数字)均为正整数。
+     * 元素组合(a1, a2, … , ak)必须是非降序(ie, a1 ≤ a2 ≤ … ≤ ak)。
+     * 解集不能包含重复的组合。
+     *
+     * @param num:    Given the candidate numbers
+     * @param target: Given the target number
+     * @return: All the combinations that sum to target
+     */
+    public List<List<Integer>> combinationSum2(int[] num, int target) {
+        // write your code here
+        List<List<Integer>> rtn = new ArrayList<>();
+        Arrays.sort(num);
+        List<Integer> unSelectList = new ArrayList<>(num.length);
+        for (int i = 0; i < num.length; i++) {
+            unSelectList.add(i);
+        }
+        combinationSum2(rtn, num, unSelectList, new ArrayList<Integer>(), 0, target);
+        return rtn;
+    }
+
+    private void combinationSum2(List<List<Integer>> rtn, int[] A, List<Integer> unSelectList, List<Integer> selectList, int nowSum, int target) {
+        if (nowSum == target) {
+            List<Integer> list = new ArrayList<>();
+            for (Integer i : selectList) {
+                list.add(A[i]);
+            }
+            rtn.add(list);
+        }
+        if (nowSum > target) {
+            return;
+        }
+
+        Set<Integer> addCache = new TreeSet<>();
+        for (int i = 0; i < unSelectList.size(); i++) {
+            if (!addCache.contains(A[unSelectList.get(i)]) && (selectList.size() == 0 || unSelectList.get(i) > selectList.get(selectList.size() - 1))) {
+                combinationSum2(rtn, A, removeList(unSelectList, i), addList(selectList, unSelectList.get(i)), nowSum + A[unSelectList.get(i)], target);
+                addCache.add(A[unSelectList.get(i)]);
+            }
+        }
+    }
+
+    /**
+     * 535. 打劫房屋 III
+     * 中文English
+     * 在上次打劫完一条街道之后和一圈房屋之后，窃贼又发现了一个新的可以打劫的地方，但这次所有的房子组成的区域比较奇怪，聪明的窃贼考察地形之后，发现这次的地形是一颗二叉树。与前两次偷窃相似的是每个房子都存放着特定金额的钱。你面临的唯一约束条件是：相邻的房子装着相互联系的防盗系统，且当相邻的两个房子同一天被打劫时，该系统会自动报警。
+     * <p>
+     * 算一算，如果今晚去打劫，你最多可以得到多少钱，当然在不触动报警装置的情况下。
+     * <p>
+     * 样例
+     * Example1
+     * <p>
+     * Input:  {3,2,3,#,3,#,1}
+     * Output: 7
+     * Explanation:
+     * Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+     * 3
+     * / \
+     * 2   3
+     * \   \
+     * 3   1
+     * Example2
+     * <p>
+     * Input:  {3,4,5,1,3,#,1}
+     * Output: 9
+     * Explanation:
+     * Maximum amount of money the thief can rob = 4 + 5 = 9.
+     * 3
+     * / \
+     * 4   5
+     * / \   \
+     * 1   3   1
+     *
+     * @param root: The root of binary tree.
+     * @return: The maximum amount of money you can rob tonight
+     */
+    public int houseRobber3(TreeNode root) {
+        // write your code here
+        return houseRobber3(root, 0, 0);
+    }
+
+    private int houseRobber3(TreeNode node, int lSum, int rSum) {
+        if (null == node) {
+            return 0;
+        }
+        int ll = lSum + houseRobber3(node.left, 0, 0);
+        int lr = rSum + houseRobber3(node.right, 0, 0);
+        return Math.max(node.val + Math.max(houseRobber3(node.left.left, 0, 0), houseRobber3(node.left.right, 0, 0)) + Math.max(houseRobber3(node.right.left, 0, 0), houseRobber3(node.right.right, 0, 0)), ll + lr);
+    }
+
+    /**
+     * 14. 二叉树的最长连续子序列 II
+     * 中文English
+     * 给定一棵二叉树，找到最长连续序列路径的长度(节点数)。
+     * 路径起点跟终点可以为二叉树的任意节点。
+     * <p>
+     * 样例
+     * 例1:
+     * <p>
+     * 输入:
+     * {1,2,0,3}
+     * 输出:
+     * 4
+     * 解释:
+     * 1
+     * / \
+     * 2   0
+     * /
+     * 3
+     * 0-1-2-3
+     * 例2:
+     * <p>
+     * 输入:
+     * {3,2,2}
+     * 输出:
+     * 2
+     * 解释:
+     * 3
+     * / \
+     * 2   2
+     * 2-3
+     *
+     * @param root: the root of binary tree
+     * @return: the length of the longest consecutive sequence path
+     */
+    public int longestConsecutive2(TreeNode root) {
+        // write your code here
+        Set<Integer> treeSet = new TreeSet<>();
+        bfs(treeSet, root);
+        int last=Integer.MIN_VALUE;
+        int length = 1;
+        int maxLength = 0;
+        for (Integer i : treeSet) {
+            if(i-last==1){
+                length++;
+            }else{
+                maxLength = Math.max(length,maxLength);
+                length=1;
+            }
+            last=i;
+        }
+        return Math.max(maxLength,length);
+    }
+
+    private void bfs(Set<Integer> treeSet, TreeNode node) {
+        if (null == node) {
+            return;
+        }
+        treeSet.add(node.val);
+        bfs(treeSet, node.left);
+        bfs(treeSet, node.right);
+    }
+
     public static void main(String[] args) {
-        System.out.println(new CatGoldSolution().kSumII(new int[]{1, 2, 3, 4}, 2, 5));
+        TreeNode root=new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(0);
+        System.out.println(new CatGoldSolution().longestConsecutive2(root));
 
     }
 
