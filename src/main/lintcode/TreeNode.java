a4d65cc26ae6eb1857da554e332bae9b92ff58e5
zhangmeng <zhangmeng@bikeman.info>
2019-04-15 11:53:36 +0800

    优化效率

--- src/main/lintcode/TreeNode.java
+++ src/main/lintcode/TreeNode.java
@@ -1,5 +1,10 @@
 package lintcode;
 
+import com.mysql.cj.util.StringUtils;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     public int val;
     public TreeNode left, right;
@@ -9,8 +14,42 @@ public class TreeNode {
         this.left = this.right = null;
     }
 
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
     public TreeNode(String nodeArray) {
         String[] nodes = nodeArray.split(",");
+        Queue<TreeNode> treeNodeQueue = new LinkedList();
+        for (int i = 0; i < nodes.length; i++) {
+            if (StringUtils.isEmptyOrWhitespaceOnly(nodes[i])) {
+                continue;
+            }
+            if (treeNodeQueue.isEmpty()) {
+                this.val = Integer.parseInt(nodes[i]);
+                treeNodeQueue.add(this);
+            } else {
+                TreeNode treeNode = treeNodeQueue.poll();
+                if (StringUtils.isStrictlyNumeric(nodes[i])) {
+                    treeNode.left = new TreeNode(Integer.parseInt(nodes[i]));
+                    treeNodeQueue.add(treeNode.left);
+                }
+                if (++i < nodes.length && StringUtils.isStrictlyNumeric(nodes[i])) {
+                    treeNode.right = new TreeNode(Integer.parseInt(nodes[i]));
+                    treeNodeQueue.add(treeNode.right);
+                }
+            }
 
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode("1,2,#,3,4,5,#,7");
+        System.out.println(node);
     }
 }
